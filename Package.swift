6ed72b519bae6b6367f706b92797afc51224646e
Adam Mordavsky <mordavsky.a@gmail.com>
2025-08-18 16:01:07 +0200

    Release 1.0.1

--- Package.swift
+++ Package.swift
@@ -32,13 +32,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PrebidXCFramework",
-            url: "https://github.com/RingierAdvertising/tagmanager-sdk-ios/releases/download/1.0.0/PrebidMobile.xcframework.zip",
+            url: "https://github.com/RingierAdvertising/tagmanager-sdk-ios/releases/download/1.0.1/PrebidMobile.xcframework.zip",
             checksum: "a168cc1fec46cafa454ba8f09b5b6459f0b4efc09cfaf9d4dad87ce5fc109e6a"
         ),
         .binaryTarget(
             name: "RingierAdXCFramework",
-            url: "https://github.com/RingierAdvertising/tagmanager-sdk-ios/releases/download/1.0.0/RingierAdSDK.xcframework.zip",
-            checksum: "fa0bbfcb1b0f5628504ac741505b5da5ae3d5efbbad868dc45b574d6ef398348"
+            url: "https://github.com/RingierAdvertising/tagmanager-sdk-ios/releases/download/1.0.1/RingierAdSDK.xcframework.zip",
+            checksum: "c380a99492bc4a6fe4c53bbbd11977f4d0aa90ccc80efcca421e6654df56bf6b"
         )
     ],
     swiftLanguageVersions: [.v5]
