f50dd2d7e30d1fe649a86831fdadee5198c1e280
Adam Mordavsky <mordavsky.a@gmail.com>
2025-08-18 15:24:10 +0200

    Release 1.0.0

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "RingierAd",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "RingierAdSDK",
+            targets: [
+                "RingierAdSDK"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/appnexus/mobile-sdk-ios-spm", exact: "9.1.1"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", exact: "11.2.0")
+    ],
+    targets: [
+        .target(
+            name: "RingierAdSDK",
+            dependencies: [
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
+                .product(name: "AppNexusSDK", package: "mobile-sdk-ios-spm"),
+                .target(name: "PrebidXCFramework"),
+                .target(name: "RingierAdXCFramework")
+            ],
+            path: "Sources"
+        ),
+        .binaryTarget(
+            name: "PrebidXCFramework",
+            url: "https://github.com/RingierAdvertising/tagmanager-sdk-ios/releases/download/1.0.0/PrebidMobile.xcframework.zip",
+            checksum: "a168cc1fec46cafa454ba8f09b5b6459f0b4efc09cfaf9d4dad87ce5fc109e6a"
+        ),
+        .binaryTarget(
+            name: "RingierAdXCFramework",
+            url: "https://github.com/RingierAdvertising/tagmanager-sdk-ios/releases/download/1.0.0/RingierAdSDK.xcframework.zip",
+            checksum: "fa0bbfcb1b0f5628504ac741505b5da5ae3d5efbbad868dc45b574d6ef398348"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
